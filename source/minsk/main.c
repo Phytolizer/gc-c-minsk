d3019df660ce29dea56a6e791a10031834a501d5
Phytolizer <kyle.coffey@tamu.edu>
2021-08-30 23:40:52 -0500

    REPL

--- source/minsk/main.c
+++ source/minsk/main.c
@@ -1,7 +1,55 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+
+#include <gc.h>
+
+char* input_line(const char* prompt)
+{
+  printf("%s", prompt);
+  char* line = NULL;
+  size_t linelen = 0;
+  while (true)
+  {
+    char c = fgetc(stdin);
+    if (c == EOF)
+    {
+      return NULL;
+    }
+    if (c == '\n')
+    {
+      c = '\0';
+    }
+    ++linelen;
+    line = GC_REALLOC(line, linelen);
+    line[linelen - 1] = c;
+    if (c == '\0')
+    {
+      break;
+    }
+  }
+  return line;
+}
 
 int main(void)
 {
-  printf("Hello, world!\n");
+  while (true)
+  {
+    char* line = input_line("> ");
+    if (!line)
+    {
+      printf("\n");
+      break;
+    }
+
+    if (strcmp(line, "1 + 2 * 3") == 0)
+    {
+      printf("7\n");
+    }
+    else
+    {
+      printf("ERROR: Invalid expression!\n");
+    }
+  }
   return 0;
 }
