a3e79ecf2dd968a13697d013c4e1ef04d760daca
Phytolizer <kyle.coffey@tamu.edu>
2021-09-05 08:52:49 -0500

    Add unary operators to parser tests

--- source/tests/CodeAnalysis/Syntax/ParserTests.cpp
+++ source/tests/CodeAnalysis/Syntax/ParserTests.cpp
@@ -102,3 +102,55 @@ TEST_CASE("parser: binary expression honors precedences")
     }
   }
 }
+
+static std::vector<std::pair<SyntaxKind, SyntaxKind>> get_unary_operator_pairs()
+{
+  std::vector<std::pair<SyntaxKind, SyntaxKind>> binary_operator_pairs;
+  for (auto k1 : get_unary_operator_kinds())
+  {
+    for (auto k2 : get_binary_operator_kinds())
+    {
+      binary_operator_pairs.emplace_back(k1, k2);
+    }
+  }
+  return binary_operator_pairs;
+}
+
+TEST_CASE("parser: unary expression honors precedences")
+{
+  for (auto& test : get_unary_operator_pairs())
+  {
+    int unary_precedence = unary_operator_precedence(test.first);
+    int binary_precedence = binary_operator_precedence(test.second);
+    sds unary_text = syntax_facts_get_text(test.first);
+    sds binary_text = syntax_facts_get_text(test.second);
+    sds text = sdscatfmt(sdsempty(), "%S a %S b", unary_text, binary_text);
+    SyntaxNode* expression
+        = reinterpret_cast<SyntaxNode*>(syntax_tree_parse(text)->root);
+
+    if (unary_precedence >= binary_precedence)
+    {
+      AssertingEnumerator e{expression};
+      e.assert_node(SYNTAX_KIND_BINARY_EXPRESSION);
+      e.assert_node(SYNTAX_KIND_UNARY_EXPRESSION);
+      e.assert_token(test.first, unary_text);
+      e.assert_node(SYNTAX_KIND_NAME_EXPRESSION);
+      e.assert_token(SYNTAX_KIND_IDENTIFIER_TOKEN, "a");
+      e.assert_token(test.second, binary_text);
+      e.assert_node(SYNTAX_KIND_NAME_EXPRESSION);
+      e.assert_token(SYNTAX_KIND_IDENTIFIER_TOKEN, "b");
+    }
+    else
+    {
+      AssertingEnumerator e{expression};
+      e.assert_node(SYNTAX_KIND_UNARY_EXPRESSION);
+      e.assert_token(test.first, unary_text);
+      e.assert_node(SYNTAX_KIND_BINARY_EXPRESSION);
+      e.assert_node(SYNTAX_KIND_NAME_EXPRESSION);
+      e.assert_token(SYNTAX_KIND_IDENTIFIER_TOKEN, "a");
+      e.assert_token(test.second, binary_text);
+      e.assert_node(SYNTAX_KIND_NAME_EXPRESSION);
+      e.assert_token(SYNTAX_KIND_IDENTIFIER_TOKEN, "b");
+    }
+  }
+}
