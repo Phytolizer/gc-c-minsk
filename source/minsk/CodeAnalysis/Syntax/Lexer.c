1cf02812ddbf6789875de9f3c59624ab436f143d
Phytolizer <kyle.coffey@tamu.edu>
2021-09-05 10:40:02 -0500

    Major lexer refactoring

--- source/minsk/CodeAnalysis/Syntax/Lexer.c
+++ source/minsk/CodeAnalysis/Syntax/Lexer.c
@@ -10,6 +10,8 @@
 #include <minsk/CodeAnalysis/Syntax/SyntaxFacts.h>
 #include <minsk/CodeAnalysis/Syntax/SyntaxToken.h>
 
+#include "minsk/CodeAnalysis/Syntax/SyntaxKind.h"
+
 __attribute__((const)) static char peek(struct Lexer* lexer, int offset)
 {
   int index = lexer->position + offset;
@@ -35,6 +37,49 @@ static void next(struct Lexer* lexer)
   ++lexer->position;
 }
 
+static void read_number(struct Lexer* lexer)
+{
+  while (isdigit(current(lexer)))
+  {
+    next(lexer);
+  }
+
+  int length = lexer->position - lexer->start;
+  sds text = sdsnewlen(&lexer->text[lexer->start], length);
+  long value = strtol(text, NULL, 10);
+  if (errno == ERANGE || value < INT_MIN || value > INT_MAX)
+  {
+    diagnostic_bag_report_invalid_number(
+        lexer->diagnostics,
+        text_span_new(lexer->start, length),
+        text,
+        OBJECT_KIND_INTEGER);
+  }
+  lexer->value = OBJECT_INTEGER(value);
+  lexer->kind = SYNTAX_KIND_NUMBER_TOKEN;
+}
+
+static void read_whitespace(struct Lexer* lexer)
+{
+  while (isspace(current(lexer)))
+  {
+    next(lexer);
+  }
+  lexer->kind = SYNTAX_KIND_WHITESPACE_TOKEN;
+}
+
+static void read_identifier_or_keyword(struct Lexer* lexer)
+{
+  while (isalpha(current(lexer)))
+  {
+    next(lexer);
+  }
+
+  int length = lexer->position - lexer->start;
+  sds text = sdsnewlen(&lexer->text[lexer->start], length);
+  lexer->kind = keyword_kind(text);
+}
+
 struct Lexer* lexer_new(sds text)
 {
   struct Lexer* lexer = mc_malloc(sizeof(struct Lexer));
@@ -56,179 +101,102 @@ void lexer_free(struct Lexer* lexer)
 struct SyntaxToken* lexer_next_token(struct Lexer* lexer)
 {
   lexer->start = lexer->position;
-
-  if (isdigit(current(lexer)))
-  {
-    while (isdigit(current(lexer)))
-    {
-      next(lexer);
-    }
-
-    int length = lexer->position - lexer->start;
-    sds text = sdsnewlen(&lexer->text[lexer->start], length);
-    long value = strtol(text, NULL, 10);
-    if (errno == ERANGE || value < INT_MIN || value > INT_MAX)
-    {
-      diagnostic_bag_report_invalid_number(
-          lexer->diagnostics,
-          text_span_new(lexer->start, length),
-          text,
-          OBJECT_KIND_INTEGER);
-    }
-    return syntax_token_new(
-        SYNTAX_KIND_NUMBER_TOKEN,
-        lexer->start,
-        text,
-        OBJECT_INTEGER(value));
-  }
-
-  if (isspace(current(lexer)))
-  {
-    while (isspace(current(lexer)))
-    {
-      next(lexer);
-    }
-
-    int length = lexer->position - lexer->start;
-    sds text = sdsnewlen(&lexer->text[lexer->start], length);
-    return syntax_token_new(
-        SYNTAX_KIND_WHITESPACE_TOKEN,
-        lexer->start,
-        text,
-        OBJECT_NULL());
-  }
-
-  if (isalpha(current(lexer)))
-  {
-    while (isalpha(current(lexer)))
-    {
-      next(lexer);
-    }
-
-    int length = lexer->position - lexer->start;
-    sds text = sdsnewlen(&lexer->text[lexer->start], length);
-    enum SyntaxKind kind = keyword_kind(text);
-    return syntax_token_new(kind, lexer->start, text, OBJECT_NULL());
-  }
+  lexer->kind = SYNTAX_KIND_BAD_TOKEN;
+  lexer->value = OBJECT_NULL();
 
   switch (current(lexer))
   {
     case '+':
       lexer->position++;
-      return syntax_token_new(
-          SYNTAX_KIND_PLUS_TOKEN,
-          lexer->start,
-          sdsnew("+"),
-          OBJECT_NULL());
+      lexer->kind = SYNTAX_KIND_PLUS_TOKEN;
+      break;
     case '-':
       lexer->position++;
-      return syntax_token_new(
-          SYNTAX_KIND_MINUS_TOKEN,
-          lexer->start,
-          sdsnew("-"),
-          OBJECT_NULL());
+      lexer->kind = SYNTAX_KIND_MINUS_TOKEN;
+      break;
     case '*':
       lexer->position++;
-      return syntax_token_new(
-          SYNTAX_KIND_STAR_TOKEN,
-          lexer->start,
-          sdsnew("*"),
-          OBJECT_NULL());
+      lexer->kind = SYNTAX_KIND_STAR_TOKEN;
+      break;
     case '/':
       lexer->position++;
-      return syntax_token_new(
-          SYNTAX_KIND_SLASH_TOKEN,
-          lexer->start,
-          sdsnew("/"),
-          OBJECT_NULL());
+      lexer->kind = SYNTAX_KIND_SLASH_TOKEN;
+      break;
     case '(':
       lexer->position++;
-      return syntax_token_new(
-          SYNTAX_KIND_OPEN_PARENTHESIS_TOKEN,
-          lexer->start,
-          sdsnew("("),
-          OBJECT_NULL());
+      lexer->kind = SYNTAX_KIND_OPEN_PARENTHESIS_TOKEN;
+      break;
     case ')':
       lexer->position++;
-      return syntax_token_new(
-          SYNTAX_KIND_CLOSE_PARENTHESIS_TOKEN,
-          lexer->start,
-          sdsnew(")"),
-          OBJECT_NULL());
+      lexer->kind = SYNTAX_KIND_CLOSE_PARENTHESIS_TOKEN;
+      break;
     case '!':
       if (lookahead(lexer) == '=')
       {
         lexer->position += 2;
-        return syntax_token_new(
-            SYNTAX_KIND_BANG_EQUALS_TOKEN,
-            lexer->start,
-            sdsnew("!="),
-            OBJECT_NULL());
+        lexer->kind = SYNTAX_KIND_BANG_EQUALS_TOKEN;
       }
       else
       {
         lexer->position++;
-        return syntax_token_new(
-            SYNTAX_KIND_BANG_TOKEN,
-            lexer->start,
-            sdsnew("!"),
-            OBJECT_NULL());
+        lexer->kind = SYNTAX_KIND_BANG_TOKEN;
       }
+      break;
     case '&':
       if (lookahead(lexer) == '&')
       {
         lexer->position += 2;
-        return syntax_token_new(
-            SYNTAX_KIND_AMPERSAND_AMPERSAND_TOKEN,
-            lexer->start,
-            sdsnew("&&"),
-            OBJECT_NULL());
+        lexer->kind = SYNTAX_KIND_AMPERSAND_AMPERSAND_TOKEN;
       }
+      break;
     case '|':
       if (lookahead(lexer) == '|')
       {
         lexer->position += 2;
-        return syntax_token_new(
-            SYNTAX_KIND_PIPE_PIPE_TOKEN,
-            lexer->start,
-            sdsnew("||"),
-            OBJECT_NULL());
+        lexer->kind = SYNTAX_KIND_PIPE_PIPE_TOKEN;
       }
+      break;
     case '=':
       if (lookahead(lexer) == '=')
       {
         lexer->position += 2;
-        return syntax_token_new(
-            SYNTAX_KIND_EQUALS_EQUALS_TOKEN,
-            lexer->start,
-            sdsnew("=="),
-            OBJECT_NULL());
+        lexer->kind = SYNTAX_KIND_EQUALS_EQUALS_TOKEN;
       }
       else
       {
         lexer->position++;
-        return syntax_token_new(
-            SYNTAX_KIND_EQUALS_TOKEN,
-            lexer->start,
-            sdsnew("="),
-            OBJECT_NULL());
+        lexer->kind = SYNTAX_KIND_EQUALS_TOKEN;
       }
+      break;
     case '\0':
-      return syntax_token_new(
-          SYNTAX_KIND_END_OF_FILE_TOKEN,
+      lexer->kind = SYNTAX_KIND_END_OF_FILE_TOKEN;
+      break;
+    case '0' ... '9':
+      read_number(lexer);
+      break;
+    case ' ':
+    case '\t':
+    case '\n':
+    case '\r':
+      read_whitespace(lexer);
+      break;
+    case 'a' ... 'z':
+    case 'A' ... 'Z':
+      read_identifier_or_keyword(lexer);
+      break;
+    default:
+      diagnostic_bag_report_bad_character(
+          lexer->diagnostics,
           lexer->start,
-          sdsnew(""),
-          OBJECT_NULL());
+          current(lexer));
+      lexer->position++;
   }
 
-  diagnostic_bag_report_bad_character(
-      lexer->diagnostics,
-      lexer->start,
-      current(lexer));
-  lexer->position++;
-  return syntax_token_new(
-      SYNTAX_KIND_BAD_TOKEN,
-      lexer->start,
-      sdscatlen(sdsempty(), &lexer->text[lexer->start], 1),
-      OBJECT_NULL());
+  int length = lexer->position - lexer->start;
+  sds text = syntax_facts_get_text(lexer->kind);
+  if (!text)
+  {
+    text = sdsnewlen(&lexer->text[lexer->start], length);
+  }
+  return syntax_token_new(lexer->kind, lexer->start, text, lexer->value);
 }
