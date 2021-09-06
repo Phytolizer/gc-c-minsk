08e609b0d52d218e7131c92740b72290a32e8c39
Phytolizer <kyle.coffey@tamu.edu>
2021-09-06 05:59:31 -0500

    test SourceText

---
+++ source/tests/CodeAnalysis/Text/SourceTextTests.cpp
@@ -0,0 +1,30 @@
+#include <array>
+#include <doctest/doctest.h>
+#include <string>
+extern "C" {
+#include <minsk/CodeAnalysis/Text/SourceText.h>
+}
+
+struct SourceTextIncludesLastLineTest
+{
+  std::string text;
+  int expected_line_count;
+};
+
+const std::array SOURCE_TEXT_INCLUDES_LAST_LINE_TESTS = {
+    SourceTextIncludesLastLineTest{".", 1},
+    SourceTextIncludesLastLineTest{".\r\n", 2},
+    SourceTextIncludesLastLineTest{".\r\n\r\n", 3},
+};
+
+TEST_SUITE("SourceText")
+{
+  TEST_CASE("includes last line")
+  {
+    for (auto& test : SOURCE_TEXT_INCLUDES_LAST_LINE_TESTS)
+    {
+      auto* source_text = source_text_from(sdsnew(test.text.c_str()));
+      CHECK(source_text->lines->length == test.expected_line_count);
+    }
+  }
+}
